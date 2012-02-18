de1abafa0ac791d6b64e25f643a688cf1b66e139
Kenton Hamaluik <kenton@hamaluik.com>
2012-02-17 18:41:45 -0700

    skeleton

--- src/com/mcnsa/instanceportals/InstancePortals.java
+++ src/com/mcnsa/instanceportals/InstancePortals.java
@@ -1,5 +1,68 @@
 package com.mcnsa.instanceportals;
 
-public class InstancePortals {
+import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.tehkode.permissions.PermissionManager;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+public class InstancePortals extends JavaPlugin {
+	// load the minecraft logger
+	Logger log = Logger.getLogger("Minecraft");
+
+	// keep track of permissions
+	public PermissionManager permissions = null;
+
+	public void onEnable() {
+		// set up permissions
+		this.setupPermissions();
+		
+		
+		log("plugin enabled!");
+	}
+	
+	public void onDisable() {
+		log("plugin disabled!");
+	}
+
+	// for simpler logging
+	public void log(String info) {
+		log.info("[InstancePortals] " + info);
+	}
+
+	// for error reporting
+	public void error(String info) {
+		log.info("[InstancePortals] <ERROR> " + info);
+	}
+
+	// for debugging
+	// (disable for final release)
+	public void debug(String info) {
+		log.info("[InstancePortals] <DEBUG> " + info);
+	}
+
+	// load the permissions plugin
+	public void setupPermissions() {
+		if(Bukkit.getServer().getPluginManager().isPluginEnabled("PermissionsEx")) {
+			this.permissions = PermissionsEx.getPermissionManager();
+			log("permissions successfully loaded!");
+		}
+		else {
+			error("PermissionsEx not found!");
+		}
+	}
+
+	// just an interface function for checking permissions
+	// if permissions are down, default to OP status.
+	public boolean hasPermission(Player player, String permission) {
+		if(permissions != null) {
+			return permissions.has(player, "mcnsachat2." + permission);
+		}
+		else {
+			return player.isOp();
+		}
+	}
 }
